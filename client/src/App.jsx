38f9c208ce267a87f4caaf8c165ef462f52a1cb6
Rahul Kumar <rahulkumarjha58978@gmail.com>
2024-05-17 18:24:41 +0530

    added routes and make dummy pages and config tailwind

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Signin from './pages/Signin'
+import Signup from './pages/Signup'
+import Home from './pages/Home'
+import Profile from './pages/Profile'
+import About from './pages/About'
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <h1>rahul</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/signin' element={<Signin />}></Route>
+        <Route path='/signup' element={<Signup />}></Route>
+        <Route path='/about' element={<About />}></Route>
+        <Route path='/profile' element={<Profile />}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
