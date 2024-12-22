8ed6ab06c52358f5b88a5e85747dba74b04262af
Rahul Kumar <rahulkumarjha58978@gmail.com>
2024-12-22 12:00:39 +0530

    add preproduction changes in packagejson and index.js

--- api/index.js
+++ api/index.js
@@ -5,15 +5,20 @@ import cors from 'cors'
 import UserRoutes from './routes/UserRoutes.js'
 import AuthRoutes from './routes/AuthRoutes.js'
 import cookieParser from 'cookie-parser'
+import path from 'path'
 dotenv.config();
 //INITIALIZIING THE APPLICATION
 const app=express();
 //DEFINING PORT NO
 const PORT=4000;
+const __dirname=path.resolve();
 app.use(cors({credentials:true,origin:'http://localhost:5173'}));
 app.use(express.json())
 app.use(cookieParser());
-
+app.use(express.static(path.join(__dirname,"/client/dist")));
+app.get('*',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,"client","dist","index.html"))
+})
 // console.log(PORT)
 //SERVER CONNECTED!!1
 app.listen(PORT,()=>{
